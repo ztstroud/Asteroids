0bee6923cf44929596820b72356d2627d497ac5b
ztstroud <ztstroud@gmail.com>
2017-12-04 15:59:25 -0700

    Created basic login form

---
+++ Asteroids/Login.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Asteroids
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hostInput = new System.Windows.Forms.TextBox();
+            this.nameInput = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.hostLabel = new System.Windows.Forms.Label();
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // hostInput
+            // 
+            this.hostInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.hostInput.Location = new System.Drawing.Point(29, 68);
+            this.hostInput.Name = "hostInput";
+            this.hostInput.Size = new System.Drawing.Size(429, 41);
+            this.hostInput.TabIndex = 0;
+            this.hostInput.Text = "localhost";
+            // 
+            // nameInput
+            // 
+            this.nameInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.nameInput.Location = new System.Drawing.Point(29, 171);
+            this.nameInput.Name = "nameInput";
+            this.nameInput.Size = new System.Drawing.Size(429, 41);
+            this.nameInput.TabIndex = 1;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.nameLabel.Location = new System.Drawing.Point(23, 132);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(92, 36);
+            this.nameLabel.TabIndex = 2;
+            this.nameLabel.Text = "Name";
+            // 
+            // hostLabel
+            // 
+            this.hostLabel.AutoSize = true;
+            this.hostLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.hostLabel.Location = new System.Drawing.Point(23, 29);
+            this.hostLabel.Name = "hostLabel";
+            this.hostLabel.Size = new System.Drawing.Size(76, 36);
+            this.hostLabel.TabIndex = 3;
+            this.hostLabel.Text = "Host";
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.confirmButton.Location = new System.Drawing.Point(160, 266);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(167, 58);
+            this.confirmButton.TabIndex = 4;
+            this.confirmButton.Text = "Confirm";
+            this.confirmButton.UseVisualStyleBackColor = true;
+            this.confirmButton.Click += new System.EventHandler(this.confirmButton_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 336);
+            this.Controls.Add(this.confirmButton);
+            this.Controls.Add(this.hostLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.nameInput);
+            this.Controls.Add(this.hostInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox hostInput;
+        private System.Windows.Forms.TextBox nameInput;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label hostLabel;
+        private System.Windows.Forms.Button confirmButton;
+    }
+}
---
+++ Asteroids/Login.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Asteroids
+{
+    public partial class Login : Form
+    {
+        /// <summary>
+        /// Called when the login dialog has been completed, returning the host
+        /// and name entered by the user.
+        /// </summary>
+        /// 
+        /// <param name="host">the host given by the user</param>
+        /// <param name="name">the name given by the user</param>
+        public delegate void Completed(string host, string name);
+
+        /// <summary>
+        /// Used to check if a given name is valid.
+        /// </summary>
+        /// 
+        /// <param name="name">the name to validate</param>
+        /// 
+        /// <returns>true, iff the given name is valid, otherwise false</returns>
+        public delegate bool ValidateName(string name);
+
+        private Completed completed;
+        private ValidateName validateName;
+
+        public Login(Completed completed, ValidateName validateName)
+        {
+            InitializeComponent();
+
+            this.completed = completed ?? throw new ArgumentNullException("Login dialog cannot have a null complete delegate");
+            this.validateName = validateName ?? (s => true);
+        }
+
+        private void confirmButton_Click(object sender, EventArgs e)
+        {
+            if(hostInput.Text.Length == 0)
+            {
+                MessageBox.Show("You need to enter a host", "Error");
+            }
+            else if(!validateName(nameInput.Text))
+            {
+                MessageBox.Show("The name you entered is not valid", "Error");
+            }
+            else
+            {
+                completed(hostInput.Text, nameInput.Text);
+                Close();
+            }
+        }
+    }
+}
